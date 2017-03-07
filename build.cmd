d6bfb1dfdfc783d958b9b96fadb6231800f46e07
Adam Straughan <adam.straughan@gmail.com>
2017-03-07 16:58:09 +0000

    enhance build to create home,end,prev,next

--- build.cmd
+++ build.cmd
@@ -21,13 +21,35 @@ pushd %DEST%
 git init
 popd
 set part=0
+
+pushd %DEST%
+:create home command
+echo @git co . > home.cmd
+echo @git co part0 >> home.cmd
+:create end command
+echo @git co . > end.cmd
+echo @git co master >> end.cmd
+popd
+
 for /F "tokens=*" %%A in (index.txt) do (
     copy %%A %DEST%
     pushd %DEST%
+    
+    :create next command
+    set /a next=!part!+1
+    echo @git co . > next.cmd
+    echo @git co part!next! >> next.cmd
+    :create prev command
+    set /a prev=!part!-1
+    echo @git co . > prev.cmd
+    echo @git co part!prev! >> prev.cmd
+
     git add .
     git commit -m "Part !part!: %%A"
     git branch part!part!
+
     set /a part+=1
+
     popd
 )
 :END
